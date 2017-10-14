f984ebf2f93f81f76b230ab312fdec83a4c87d77
Michael Wolfe <michael.wolfe101@gmail.com>
2017-10-14 10:01:42 -0400

    added styles.css and fixed next arrival bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -97,5 +97,5 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
     // Add each train's data into the table
     $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" +
-        frequency + "</td><td>" + nextArrival + "</td><td>" + minsAway + "</td></tr>");
+        frequency + "</td><td>" + moment(nextArrival).format("HH:mm") + "</td><td>" + minsAway + "</td></tr>");
 });
